a9aedf7b32c441c0838f78b0af8810a01b761ca1
Brian Gotti <40281268+bgotti@users.noreply.github.com>
2021-11-23 22:31:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ["press.hulu.com", "image.tmdb.org"],
   },
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
